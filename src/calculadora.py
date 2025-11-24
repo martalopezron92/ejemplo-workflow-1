bdd9fc12712b980c6595704a7d7f55359e96c84d
martalopezron92 <84903443+martalopezron92@users.noreply.github.com>
2025-11-24 11:44:01 +0100

    Add basic calculator functions for addition and subtraction

---
+++ src/calculadora.py
@@ -0,0 +1,11 @@
+
+def suma(a,b)
+  return a + b
+
+def resta(a,b)
+  return a - b
+
+a = 10
+b = 15
+print("La suma de ", a, " y ", b, " es: ", suma(a,b))
+print("La resta de ", a, " y ", b, " es: ", resta(a,b))
